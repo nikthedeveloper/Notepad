5a40c872a33b391b9d627107368a7c408aa415d4
Nikita Sirotenko <nthedeveloper@gmail.com>
2021-10-02 15:49:30 +0600

    Initial commit

---
+++ TextEditor.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace Notepad
+{
+    partial class TextEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.closeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fontDialog = new System.Windows.Forms.FontDialog();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.menuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenuItem,
+            this.formatMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileMenuItem
+            // 
+            this.fileMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openMenuItem,
+            this.saveMenuItem,
+            this.toolStripSeparator1,
+            this.closeMenuItem});
+            this.fileMenuItem.Name = "fileMenuItem";
+            this.fileMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileMenuItem.Text = "Файл";
+            // 
+            // openMenuItem
+            // 
+            this.openMenuItem.Name = "openMenuItem";
+            this.openMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.openMenuItem.Text = "Открыть...";
+            this.openMenuItem.Click += new System.EventHandler(this.openMenuItem_Click);
+            // 
+            // saveMenuItem
+            // 
+            this.saveMenuItem.Name = "saveMenuItem";
+            this.saveMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.saveMenuItem.Text = "Сохранить";
+            this.saveMenuItem.Click += new System.EventHandler(this.saveMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(130, 6);
+            // 
+            // closeMenuItem
+            // 
+            this.closeMenuItem.Name = "closeMenuItem";
+            this.closeMenuItem.Size = new System.Drawing.Size(133, 22);
+            this.closeMenuItem.Text = "Закрыть";
+            this.closeMenuItem.Click += new System.EventHandler(this.closeMenuItem_Click);
+            // 
+            // formatMenuItem
+            // 
+            this.formatMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontMenuItem});
+            this.formatMenuItem.Name = "formatMenuItem";
+            this.formatMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.formatMenuItem.Text = "Формат";
+            // 
+            // fontMenuItem
+            // 
+            this.fontMenuItem.Name = "fontMenuItem";
+            this.fontMenuItem.Size = new System.Drawing.Size(113, 22);
+            this.fontMenuItem.Text = "Шрифт";
+            this.fontMenuItem.Click += new System.EventHandler(this.fontMenuItem_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox.Location = new System.Drawing.Point(0, 24);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(800, 426);
+            this.textBox.TabIndex = 1;
+            this.textBox.Text = "";
+            this.textBox.TextChanged += new System.EventHandler(this.textBox_TextChanged);
+            // 
+            // TextEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "TextEditorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Новый документ";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem closeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem formatMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fontMenuItem;
+        private System.Windows.Forms.FontDialog fontDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.RichTextBox textBox;
+    }
+}
+
---
+++ TextEditor.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    public partial class TextEditorForm : Form
+    {
+        #region Vars
+        private string fileName;
+        private bool isEdit;
+        #endregion  
+
+        private string FileName { get { return fileName; } set { fileName = value; } }
+        private bool IsEdit { get { return isEdit; } set { isEdit = value; } }
+
+        public TextEditorForm()
+        {
+            InitializeComponent();
+            FileName = " ";
+            
+        }
+
+        private void openMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Text = openFileDialog.FileName;
+                FileName = openFileDialog.FileName;
+                textBox.Text = File.ReadAllText(FileName);
+            }
+            else
+            {
+                Text = "Новый документ";
+            }
+        }
+
+        private void saveMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!FileName.Contains(" "))
+            {
+                File.WriteAllText(FileName, textBox.Text);
+            }
+            else
+            {
+                saveFileDialog.ShowDialog();
+                File.WriteAllText(saveFileDialog.FileName + ".txt", textBox.Text);
+                this.Text = saveFileDialog.FileName;
+            }
+        }
+
+        private void closeMenuItem_Click(object sender, EventArgs e)
+        {
+            if (!IsEdit)
+            {
+                this.Close();
+            }
+            else
+            {
+                if (!FileName.Contains(" "))
+                {
+                    File.WriteAllText(FileName, textBox.Text);
+                }
+                else
+                {
+                    saveFileDialog.ShowDialog();
+                    File.WriteAllText(saveFileDialog.FileName + ".txt", textBox.Text);
+                    this.Text = saveFileDialog.FileName;
+                }
+            }
+        }
+
+        private void fontMenuItem_Click(object sender, EventArgs e)
+        {
+            fontDialog.ShowDialog();
+            textBox.Font = fontDialog.Font;
+        }
+
+        private void textBox_TextChanged(object sender, EventArgs e)
+        {
+            IsEdit = true;
+        }
+    }
+}
